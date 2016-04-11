222fb874a1db72b3f18a62bbba691676bfcea34e
mrfabroa <eric.fabroa@ycdsbk12.ca>
2016-04-11 09:45:55 -0400

    address and rectangle examples

--- OOP/2_1_classes_attributes.py
+++ OOP/2_1_classes_attributes.py
@@ -12,7 +12,6 @@ class Point(object):
         self.x = 0
         self.y = 0
 
-
 class Rectangle(object):
     """
     A object that models a rectangle
@@ -27,6 +26,19 @@ class Rectangle(object):
 
         self.width = 0
         self.height = 0
+
+class Address(object):
+    """
+    Models an Address object with attributes for a street address
+    """
+
+    def __init__(self):
+        self.address_line1 = ""
+        self.address_line2 = ""
+        self.postal_code = ""
+        self.province = ""
+        self.city = ""
+        self.country = "Canada"
 
 
 def point_demo():
@@ -44,7 +56,6 @@ def point_demo():
     point1.y = 3
 
     print point1.x, point1.y
-
 
 def point_distance():
     """
@@ -68,6 +79,50 @@ def point_distance():
     distance = sqrt((point2.y - point1.y)**2 + (point2.x - point1.x)**2)
     return distance
 
-print point_distance()
+def rect_area(width, height):
+
+    # create an instance of a rectangle
+    tile = Rectangle()
+    tile.width = width
+    tile.height = height
+
+    # compute and return rectangle area
+    return tile.width * tile.height
+
+
+def print_address(addr):
+
+    addr_str = addr.address_line1
+    if addr.address_line2 != "":
+        addr_str += "\n" + addr.address_line2
+    addr_str += "\n" + addr.city
+    addr_str += "\n" + addr.postal_code
+    addr_str += "\n" + addr.province
+    addr_str += "\n" + addr.country
+
+    print addr_str
+
+def address_demo():
+
+    # create one address object - work
+    work = Address()
+    work.address_line1 = "123 Main St."
+    work.address_line2 = "Unit 2B"
+    work.postal_code = "L3T 7P1"
+    work.city = "Thornhill"
+    work.province = "Ontario"
+
+    # create a home address object
+    home = Address()
+    home.address_line1 = "578 Bayview Ave."
+    home.postal_code = "L4G 7T4"
+    home.city = "Aurora"
+    home.province = "Ontario"
+
+    print_address(home)
+    print ""
+    print_address(work)
+
+address_demo()
 
 
