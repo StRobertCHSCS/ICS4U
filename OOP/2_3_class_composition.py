d7cc8b34dafe7ff1e3b6794ed36ff48e8c767e43
mrfabroa <eric.fabroa@ycdsbk12.ca>
2016-04-20 09:52:00 -0400

    list composition examples

--- OOP/2_3_class_composition.py
+++ OOP/2_3_class_composition.py
@@ -1,6 +1,6 @@
 __author__ = 'eric'
 
-class Point():
+class Point(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -45,6 +45,44 @@ class Rectangle(object):
         self.corner.printMe()
 
 
+class Student(object):
+
+    def __init__(self, firstName, lastName, mark):
+        """
+        Initialize the student
+        """
+        self.firstName = firstName
+        self.lastName = lastName
+        self.mark = mark
+
+class Classroom(object):
+
+    def __init__(self, coursecode, roomNumber):
+        self.coursecode = coursecode
+        self.roomNumber = roomNumber
+        self.students = []  #to contain Student objects
+
+
+    def get_classAverage(self):
+        """
+        return the average mark of the students
+        """
+
+        total = 0
+
+        #sum up the marks
+        for student in self.students:
+            total += student.mark
+
+        return float(total)/len(self.students)
+
+
+    def add_student(self, firstName, lastName, mark):
+
+        self.students.append(Student(firstName, lastName, mark))
+
+
+
 def example1_c():
     """
     Demonstrate the creation of a Rectangle object and set it's corner to a new point
@@ -55,4 +93,41 @@ def example1_c():
     tile.set_corner(20,40)
     tile.show_corner()
 
-example1_c()
+
+def example2():
+    """
+    Create five Student objects.  Set their firstName, lastName and mark attributes.
+    Create a Classroom object and append the student objects to the students attribute of the Classroom object
+    print the class average of the classroom.
+
+    :return: None
+    """
+
+    fab_students = [Student("Carlo", "Macha", 21)]
+    fab_students.append(Student("Marino", "V", 99.4))
+    fab_students.append(Student("Alena", "Calma", 13))
+    fab_students.append(Student("Vince", "Mak", 100))
+    fab_students.append(Student("Colin", "Wong", 96))
+
+    # create a classroom
+    fab_247 = Classroom("ICS4U", "247")
+
+    # add students to the classroom
+    for s in fab_students:
+        fab_247.students.append(s)
+
+    print fab_247.get_classAverage()
+
+def example2_1():
+    """
+    uses the add_student method
+    :return:
+    """
+
+    # create a classroom
+    fab_247 = Classroom("ICS4U", "247")
+    fab_247.add_student("Carlo", "Macha", 21)
+    fab_247.add_student("Marino", "V", 99.4)
+    fab_247.add_student("Alena", "Calma", 13)
+    fab_247.add_student("Vince", "Mak", 100)
+    fab_247.add_student("Colin", "Wong", 96)
